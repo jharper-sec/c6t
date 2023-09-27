5144f9fa53bafc10b72c0d7c32f37d9919067b82
Jonathan Harper <39912347+jharper-sec@users.noreply.github.com>
2023-09-27 07:44:19 -0400

    Updated author email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Unofficial Contrast CLI",
     url="https//github.com/jharper-sec/c6t",
     author="Jonathan Harper",
-    author_email="jonnydharper@gmail.com",
+    author_email="jonathan.harper@contrastsecurity.com",
     license="Apache 2.0",
     packages=["c6t"],
     zip_safe=False,
